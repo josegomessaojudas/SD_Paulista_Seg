af0d860963966daedf0ce5aa33ce039abb0bf58d
josegomessaojudas <111508682+josegomessaojudas@users.noreply.github.com>
2022-09-13 06:46:56 -0300

    final_aula_12_09

---
+++ aula_09_12/consulta/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/lembretes", (req, res) => { });
+
+app.post("/eventos", (req, res) => { });
+
+app.listen(6000, () => console.log("Consultas. Porta 6000"));
---
+++ aula_09_12/lembretes/index.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+const lembretes = {};
+const axios = require("axios");
+contador = 0;
+
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes);
+});
+
+app.put('/lembretes', async (req, res) => {
+    contador++;
+    const { texto } = req.body;
+    lembretes[contador] = {
+        contador,
+        texto
+    }
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        },
+    });
+    res.status(201).send(lembretes[contador]);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
+});
+
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000');
+});
